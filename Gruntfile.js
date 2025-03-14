78ce5412223a9d83699b2eb4905c9eb9d0c86d52
samuel <samuelciole@gmail.com>
2025-03-14 14:37:53 -0300

    subindo

---
+++ Gruntfile.js
@@ -0,0 +1,33 @@
+module.exports = function (grunt){
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        less: {
+            development:{
+                files:{
+                    "main.css": "main.less"
+                }
+            }
+        },
+        uglify:{
+            target:{
+                files:{
+                    'dist/scripts/main,min,js': 'src/scripts/main.js'
+                }
+            }
+
+        }
+
+
+    })
+
+    grunt.registerTask('olaGrunt', function(){
+        const done = this.async();
+        setTimeout(function(){
+            console.log('Olá Grunt');
+            done();
+        }, 3000);
+    })
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.registerTask('default', ['less', 'uglify']);
+}
---
+++ src/scripts/main.js
@@ -0,0 +1,9 @@
+(function () {
+    "use strict";
+
+    function sayHello() {
+        console.log("Olá, mundo! Este é um script usando Grunt.");
+    }
+
+    sayHello();
+})();
